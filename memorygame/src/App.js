1fb38b56f385dba4ab53e96dea13c197fb6cc3e3
Michael Dibble <mdibble93@gmail.com>
2018-07-08 15:01:20 -0400

    data.json added

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,22 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Game from "./components/ClickGame";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Memory Game</h1>
-        </header>
-        <p className="App-intro">
-          Click on an image to earn points. Click and image more than once and you lose!
-          Good Luck!
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <ClickGame />
 
 export default App;
