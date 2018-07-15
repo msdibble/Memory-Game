ff3915924df2f1c5d67e8fbc43e688f8c5fe62fd
Michael Dibble <mdibble93@gmail.com>
2018-07-15 11:52:06 -0400

    needs work with index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ClickGame from "./Components/ClickGame";
+import Header from "../src/Components/Header/Header"
 
 const App = () => (
     <div className="container-fluid">
+        <Header />
         <ClickGame />
     </div>
 );
